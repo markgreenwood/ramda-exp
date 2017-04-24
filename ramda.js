f153c5a70571c0ba2d2bce51b4c2b4a2638c2619
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-04-24 16:09:56 -0700

    Initial commit

---
+++ ramda.js
@@ -0,0 +1,16 @@
+const R = require('ramda');
+
+const obj = { a: 1, b: 2, c: 3 };
+
+const first = R.toPairs(obj);
+// const result = R.transpose(first);
+const result = R.transpose(R.toPairs(obj));
+// const result = R.map(R.objOf('match'), R.zipWith(R.objOf, R.keys(obj), R.values(obj)));
+
+// const keyValuePairs = R.ap([R.keys, R.values]);
+// console.log('First, ', keyValuePairs);
+//
+// const listOfKeyValuePairs = R.zipWith(R.objOf);
+// const result = listOfKeyValuePairs(keyValuePairs(obj));
+console.log(result);
+
