df5591502e1e0bb01ca49beed53e8c7eb2cd7d75
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-04-24 21:02:49 -0700

    Close to what I'm looking for

--- ramda.js
+++ ramda.js
@@ -4,7 +4,9 @@ const obj = { a: 1, b: 2, c: 3 };
 
 const first = R.toPairs(obj);
 // const result = R.transpose(first);
-const result = R.transpose(R.toPairs(obj));
+const lists = R.transpose(R.toPairs(obj));
+const result = R.zipWith(R.objOf)(lists[0], lists[1]);
+
 // const result = R.map(R.objOf('match'), R.zipWith(R.objOf, R.keys(obj), R.values(obj)));
 
 // const keyValuePairs = R.ap([R.keys, R.values]);
@@ -13,4 +15,3 @@ const result = R.transpose(R.toPairs(obj));
 // const listOfKeyValuePairs = R.zipWith(R.objOf);
 // const result = listOfKeyValuePairs(keyValuePairs(obj));
 console.log(result);
-
